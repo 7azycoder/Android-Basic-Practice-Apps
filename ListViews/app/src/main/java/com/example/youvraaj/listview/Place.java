a6e9ef48adc6d4d7c1c6c41c296f1de44ed6910c
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-05-12 23:10:10 +0530

    Added ListViews Basic App

---
+++ ListViews/app/src/main/java/com/example/youvraaj/listview/Place.java
@@ -0,0 +1,18 @@
+package com.example.youvraaj.listview;
+
+/**
+ * Created by Youvraaj on 12/13/2015.
+ */
+public class Place {
+    public String mNameOfPlace ;
+    public  int mZipCode;
+    public  String mNameOfImage;
+    public  String mPopup;
+
+    public  Place(String startNameOfPlace,int startZipCode,String startNameOfImage,String startPopup){
+        this.mNameOfPlace = startNameOfPlace;
+        this.mZipCode = startZipCode;
+        this.mNameOfImage = startNameOfImage;
+        this.mPopup = startPopup;
+    }
+}
---
+++ ListViews/app/src/main/java/com/example/youvraaj/listview/PlaceAdapter.java
@@ -0,0 +1,86 @@
+package com.example.youvraaj.listview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by Youvraaj on 12/13/2015.
+ */
+public class PlaceAdapter extends ArrayAdapter<Place> {
+    Context mContext;
+    int mLayoutResourceId;
+    Place mData[] = null;
+    public PlaceAdapter(Context context,int resource, Place[] data){
+        super(context,resource,data);
+        this.mContext = context;
+        this.mLayoutResourceId= resource;
+        this.mData= data;
+    }
+
+    @Override
+    public  Place getItem(int position){
+        return  super.getItem(position);
+    }
+
+    @Override
+    public View getView(int position,View convertView, ViewGroup parent){
+        View row= convertView;
+        PlaceHolder holder = null;
+
+        //if we currently dont have a row View to use
+        if(row==null){
+            //create a new View
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            row= inflater.inflate(mLayoutResourceId,parent,false);
+
+            holder = new PlaceHolder();
+
+            holder.nameView= (TextView) row.findViewById(R.id.nameTextView);
+            holder.zipView =(TextView) row.findViewById(R.id.zipcodeTextView);
+            holder.imageView = (ImageView) row.findViewById(R.id.imageView);
+
+            row.setTag(holder);
+        }else{
+            holder = (PlaceHolder) row.getTag();
+        }
+
+        //getting the data from the data array
+        Place place = mData[position];
+
+        holder.imageView.setOnClickListener(PopupListener);
+        Integer rowPosition = position;
+        holder.imageView.setTag(rowPosition);
+
+        //setting the view to reflect the data we need to display
+        holder.nameView.setText(place.mNameOfPlace);
+        holder.zipView.setText(String.valueOf(place.mZipCode)); // always pay attention to your datatypes here
+
+        //for getting the image
+        int resId = mContext.getResources().getIdentifier(place.mNameOfImage, "drawable",mContext.getPackageName());
+        holder.imageView.setImageResource(resId);
+
+        //returning the row view ( because this is called a getView after all
+        return row;
+    }
+
+    View.OnClickListener PopupListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Integer viewPosition = (Integer) v.getTag();
+            Place p= mData[viewPosition];
+            Toast.makeText(getContext(),p.mPopup,Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private static  class PlaceHolder{
+        TextView nameView;
+        TextView zipView;
+        ImageView imageView;
+    }
+}
