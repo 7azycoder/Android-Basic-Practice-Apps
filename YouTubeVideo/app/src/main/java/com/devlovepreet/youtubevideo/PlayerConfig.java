5fab615cbb2cd4a5fe08bdc14389400a01ea1ed7
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-06-24 00:26:27 +0530

    added code for adding youtube video in your app

---
+++ YouTubeVideo/app/src/main/java/com/devlovepreet/youtubevideo/PlayerConfig.java
@@ -0,0 +1,12 @@
+package com.devlovepreet.youtubevideo;
+
+/**
+ * Created by devlovepreet on 23/6/16.
+ */
+public class PlayerConfig {
+    PlayerConfig()
+    {
+
+    }
+    public static  final String API_KEY = "AIzaSyBtb2VPofHzAEIxa_R1be3VtRBj-qzdz2s";
+}
