1345debce30ff9574e469c93f7ceb4f6ef23cddf
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-06-24 15:49:05 +0530

    implemented drawer header and hamburger icon

--- NavigationDrawer/app/src/main/java/com/devlovepreet/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/com/devlovepreet/navigationdrawer/MainActivity.java
@@ -1,17 +1,33 @@
 package com.devlovepreet.navigationdrawer;
 
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
     Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
     }
 }
